6dde4d5bf91ec8e99c11e35ac7ef51110154514e
mariaed97 <msousarodrigues135@gmail.com>
2026-09-25 21:19:42 -0300

    Commit 25/09/2026

--- sistema_django_noturno/config/settings.py
+++ sistema_django_noturno/config/settings.py
@@ -75,10 +75,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_sistema_django.db',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
